eb4b4c22e118734b83ac533d7443cd4c383153b7
Mahmoud Osman <mahmoudogosman@gmail.com>
2024-09-08 01:52:36 +0100

    Made changes to the front-end of the tickets page - Changed the page's colour palette - Changed the tickets' layout from table rows to cards

--- client/src/features/tickets/Ticket.js
+++ client/src/features/tickets/Ticket.js
@@ -10,12 +10,46 @@ const Ticket = ({ ticketId }) => {
     const navigate = useNavigate()
 
     if (ticket) {
-        const created = new Date(ticket.createdAt).toLocaleString('en-US', { day: 'numeric', month: 'long' })
-        const updated = new Date(ticket.updatedAt).toLocaleString('en-US', { day: 'numeric', month: 'long' })
+        const created = new Date(ticket.createdAt).toLocaleString('en-US', { hour: 'numeric', day: '2-digit', month: '2-digit', year: 'numeric' })
+        const updated = new Date(ticket.updatedAt).toLocaleString('en-US', { hour: 'numeric', day: '2-digit', month: '2-digit', year: 'numeric' })
         const handleEdit = () => navigate(`/dash/tickets/${ticketId}`)
 
         return (
-            <tr className="table__row">
+            <div class="ticket">
+            <div class="ticket__header">
+                <div className='ticket__header__left'>
+                    <h3 class="ticket__title">{ticket.title}</h3>
+                    <h5 class="ticket__category">{ticket.category}</h5>
+                </div>
+            </div>
+            <div className='ticket__date'>
+                {!updated ? created : updated}
+            </div>
+            <hr class="ticket__divider"/>
+            <div class="ticket__urgency">
+                <div class="ticket__urgency__circle"></div>
+                <div class="ticket__urgency__circle"></div>
+                <div class="ticket__urgency__circle"></div>
+                <div class="ticket__urgency__circle"></div>
+                <div class="ticket__urgency__circle"></div>
+            </div>
+            <div class="ticket__content">
+              <div class="ticket__description">
+                <p>
+                    {ticket.description}
+                </p>
+              </div>
+            </div>
+            
+            <div class="ticket__footer">
+                <span class="ticket__footer__left">{ticket.usernames[0]}</span>
+                <span class="ticket__footer__right">{ticket.statusCode}</span>
+            </div>
+        </div>
+        
+            
+
+            /*  <tr className="table__row">
                 <td className="table__cell ticket__status">
                     {ticket.completed
                         ? <span className="ticket__status--completed">Completed</span>
@@ -35,7 +69,7 @@ const Ticket = ({ ticketId }) => {
                         <FontAwesomeIcon icon={faPenToSquare} />
                     </button>
                 </td>
-            </tr>
+            </tr> */
         )
 
     } else return null
--- client/src/features/tickets/TicketsList.js
+++ client/src/features/tickets/TicketsList.js
@@ -16,25 +16,13 @@ const TicketsList = () => {
   if (isError) {content = <p className= "errmsg">{error?.data?.message}</p>}
   if (isSuccess) {
     const { ids } = tickets
-    const tableContent = ids.length ?
+    const ticketContent = ids.length ?
       ids.map(ticketId => <Ticket key={ticketId} ticketId={ticketId} />) :
       null
     content = (
-      <table className="table table--tickets">
-        <thead className="table__thead">
-          <tr>
-            <th scope="col" className="table__th ticket__status">Username</th>
-            <th scope="col" className="table__th ticket__created">Created</th>
-            <th scope="col" className="table__th ticket__updated">Updated</th>
-            <th scope="col" className="table__th ticket__title">Title</th>
-            <th scope="col" className="table__th ticket__username">Owner</th>
-            <th scope="col" className="table__th ticket__edit">Edit</th>
-          </tr>
-        </thead>
-        <tbody>
-          {tableContent}
-        </tbody>
-      </table>
+      <div className = "ticketlist__main">
+        {ticketContent}
+      </div>
     )
   }
   return content
